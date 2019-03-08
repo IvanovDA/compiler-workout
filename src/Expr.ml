1e45eff4a0d30ca7a724c1793ae3f0f200557360
IvanovDA <st013178@student.spbu.ru>
2019-03-08 22:40:16 +0300

    HW01

--- src/Expr.ml
+++ src/Expr.ml
@@ -32,23 +32,31 @@ let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 *)
 let update x v s = fun y -> if x = y then v else s y
 
-(* An example of a non-trivial state: *)                                                   
-let s = update "x" 1 @@ update "y" 2 @@ update "z" 3 @@ update "t" 4 empty
+let boolToInt n = if n then 1 else 0
 
-(* Some testing; comment this definition out when submitting the solution. *)
-let _ =
-  List.iter
-    (fun x ->
-       try  Printf.printf "%s=%d\n" x @@ s x
-       with Failure s -> Printf.printf "%s\n" s
-    ) ["x"; "a"; "y"; "z"; "t"; "b"]
+let evalBinop op a b = match op with
+	| "+" -> a + b
+	| "-" -> a - b
+	| "*" -> a * b
+	| "/" -> a / b
+	| "%" -> a mod b
+	| "<" -> boolToInt (a < b)
+	| ">" -> boolToInt (a > b)
+	| "<=" -> boolToInt (a <= b)
+	| ">=" -> boolToInt (a >= b)
+	| "==" -> boolToInt (a == b)
+	| "!=" -> boolToInt (a != b)
+	| "&&" -> boolToInt ((a != 0) && (b != 0))
+	| "!!" -> boolToInt ((a != 0) || (b != 0))
+
 
 (* Expression evaluator
-
      val eval : state -> expr -> int
  
    Takes a state and an expression, and returns the value of the expression in 
    the given state.
 *)
-let eval = failwith "Not implemented yet"
-                    
+let rec eval s e = match e with
+  | Const n				-> n
+  | Var v				-> s v
+  | Binop (op, a, b)	-> evalBinop op (eval s a)(eval s b)  
